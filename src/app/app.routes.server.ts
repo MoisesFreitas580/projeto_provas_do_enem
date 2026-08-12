f1e844ba0320af3f6a290b27fc822c6daba00499
Moisés Freitas <moisesfreitas580@gmail.com>
2026-08-12 00:04:11 -0300

    [DEVELOPER] Modificações no código II

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  { path: 'area/:id', renderMode: RenderMode.Server },
+  { path: 'simulations/:id', renderMode: RenderMode.Server },
+  { path: 'simulations-details/:simulationId', renderMode: RenderMode.Server },
+  { path: 'attempt-sessions-exam/:examId', renderMode: RenderMode.Server },
+  { path: 'attempt-sessions-simulation/:sessionId', renderMode: RenderMode.Server },
+  { path: 'attempt-sessions-avulso/:avulsoId', renderMode: RenderMode.Server },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
